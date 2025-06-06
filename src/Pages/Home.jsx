189fb1678e3892e6494f1919bc3a3d1774e9248d
Geetha-Ragu <geetha@nura9.com>
2025-06-06 22:03:40 +0530

    updated resume

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -64,7 +64,7 @@ const Home = () => {
               type="button"
               class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
             >
-              <a href="/GEETHA P_MERN.pdf" target="_blank">
+              <a href="/GEETHA P_Frontend.pdf" target="_blank">
                 Download Resume
               </a>
             </button>
